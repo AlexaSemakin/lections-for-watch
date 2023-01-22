030fae04a935e223eb27aae11a9874ba0405b463
alex semakin <alexasemakin@gmail.com>
2023-01-22 10:15:46 +0700

    add zoom

---
+++ logic.js
@@ -0,0 +1,6 @@
+function addchange_zoom(id) {
+    document.getElementById('img').style.width = parseInt(document.getElementById('img').style.width) + 10 + '%'
+}
+function eggchange_zoom(id) {
+    document.getElementById('img').style.width = parseInt(document.getElementById('img').style.width) - 10 + '%'
+}
