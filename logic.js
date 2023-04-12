46d82e5b27dac39527ec25183d0673d83a3651df
Alex Semakin <alexasemakin@gmail.com>
2023-04-13 02:51:59 +0700

    '123'

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 function addchange_zoom(id) {
-    document.getElementById('img').style.width = parseInt(document.getElementById('img').style.width) + 10 + '%'
+    document.getElementById('img').style.width = parseInt(document.getElementById('img').style.width) + 30 + '%'
 }
 function eggchange_zoom(id) {
-    document.getElementById('img').style.width = parseInt(document.getElementById('img').style.width) - 10 + '%'
+    document.getElementById('img').style.width = parseInt(document.getElementById('img').style.width) - 30 + '%'
 }
