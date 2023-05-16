656df9cdc610bc9e582a1a102d0e3909b3a7fc23
Alex Semakin <alexasemakin@gmail.com>
2023-05-17 03:02:59 +0700

    add new photo

--- logic.js
+++ logic.js
@@ -1,6 +1,8 @@
 function addchange_zoom(id) {
-    document.getElementById('img').style.width = parseInt(document.getElementById('img').style.width) + 30 + '%'
+  document.getElementById("img").style.width =
+    parseInt(document.getElementById("img").style.width) + 30 + "%";
 }
 function eggchange_zoom(id) {
-    document.getElementById('img').style.width = parseInt(document.getElementById('img').style.width) - 30 + '%'
+  document.getElementById("img").style.width =
+    parseInt(document.getElementById("img").style.width) - 30 + "%";
 }
