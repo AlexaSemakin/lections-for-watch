c6e78a8f0c2054e591132594b623f9bec96f232e
Alex Semakin <alexasemakin@gmail.com>
2023-05-17 13:23:11 +0700

    size

--- logic.js
+++ logic.js
@@ -1,3 +1,7 @@
+function st(){
+document.getElementById("img").style.width =  500 + "%";
+}
+
 function addchange_zoom(id) {
   document.getElementById("img").style.width =
     parseInt(document.getElementById("img").style.width) + 30 + "%";
